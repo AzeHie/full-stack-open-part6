5f9f3388d85bdda160a8f9ecf716d20e8404a4fd
AzeHie <109618459+AzeHie@users.noreply.github.com>
2023-08-27 10:31:46 +0300

    Fixed to exercise 6.20, it works as it was in the theory part, just one typo in my code

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
 
   const result = useQuery({
     queryKey: ['anecdotes'],
-    queryFn: () => getAnecdotes(),
+    queryFn: getAnecdotes,
     retry: false
   });
 
--- query-anecdotes/src/requests.jsx
+++ query-anecdotes/src/requests.jsx
@@ -1,5 +1,3 @@
 import axios from 'axios';
 
-export const getAnecdotes = () => {
-  return axios.get('http://localhost:3001/anecdotes').then(res => res.data);
-};
+export const getAnecdotes = () => axios.get('http://localhost:3001/anecdotes').then(res => res.data);
